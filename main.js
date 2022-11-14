fb813f81d4d6f9ccd816fa7d126e1d4eb54661f1
D2 <102224382+Deekaks@users.noreply.github.com>
2022-11-14 12:27:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ var content=document.createElement("div");
 container.appendChild(content)
 content.className ="alert alert-sm"
 document.querySelector("#loggedin-frontpage > div:nth-child(2)").appendChild(container); 
-content.innerText ="Tänään ruokana: Nakkisoppamuusisekoitus \n\n Huomenna ruokana: 123 2347835 ";
+content.innerHTML ="Tänään ruokana: <span class=\"safkabadge\"> Nakkikastiketta (M), Perunat</span>";
