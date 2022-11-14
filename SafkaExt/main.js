8fdb54f8d11097eb2fb8006d55ff4e001f52bc10
D2 <102224382+Deekaks@users.noreply.github.com>
2022-11-14 11:58:07 +0200

    Delete SafkaExt directory

--- SafkaExt/main.js
+++
@@ -1,7 +0,0 @@
-var container=document.createElement("div");
-container.className ="panel-body no-bottom-padding no-top-padding";
-var content=document.createElement("div"); 
-container.appendChild(content)
-content.className ="alert alert-sm"
-document.querySelector("#loggedin-frontpage > div:nth-child(2)").appendChild(container); 
-content.innerText ="Tänään ruokana: Nakkisoppamuusisekoitus \n\n Huomenna ruokana: 123 2347835 ";
